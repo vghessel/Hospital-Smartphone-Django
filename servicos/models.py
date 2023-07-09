ddd65a0752b2a707199dfb3beb33c204ce52a299
Vinicius Hessel <vgerlachhessel@gmail.com>
2023-07-09 18:23:53 -0300

    listar servicos

--- servicos/models.py
+++ servicos/models.py
@@ -13,6 +13,7 @@ class Servico(models.Model):
     descricao = models.CharField(max_length=100, null=True)
     finalizado = models.BooleanField(default=False)
     protocolo = models.CharField(max_length=52, null=True, blank=True)
+    identificador = models.CharField(max_length=24, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.servico
@@ -20,4 +21,6 @@ class Servico(models.Model):
     def save(self, *args, **kwargs):
         if not self.protocolo:
             self.protocolo = datetime.now().strftime("%d/%m/%Y-%H:%M:%S-") + token_hex(16)
+        if not self.identificador:
+            self.identificador = token_urlsafe(16)
         super(Servico, self).save(*args, **kwargs)
--- servicos/views.py
+++ servicos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .forms import FormServico
+from .models import Servico
 
 
 def novo_servico(request):
@@ -15,3 +16,12 @@ def novo_servico(request):
             return HttpResponse('Salvo com sucesso')
         else:
             return render(request, 'novo_servico.html', {'form': form}) # Enviar o form preenchido para manter as infos caso o save de errado
+        
+def listar_servico(request):
+    if request.method == "GET":
+        servicos = Servico.objects.all()
+        return render(request, 'listar_servico.html', {'servicos': servicos})
+    
+def servico(request, identificador):
+    servico = get_object_or_404(Servico, identificador=identificador)
+    return render(request, 'servico.html', {'servico': servico})
