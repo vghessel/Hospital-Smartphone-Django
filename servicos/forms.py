da2a13b12f824f2312490caaf074c9ad7d197a4e
Vinicius Hessel <vgerlachhessel@gmail.com>
2023-08-02 13:06:04 -0300

    Servicos App - Botao para adicionar novo cliente e formatacao do form de novo cliente

--- servicos/forms.py
+++ servicos/forms.py
@@ -4,11 +4,10 @@ from .models import Servico
 class FormServico(ModelForm):
     class Meta:
         model = Servico
-        exclude = ['finalizado', 'protocolo']
+        exclude = ['finalizado', 'protocolo', 'identificador']
     
     # Personalizando o HTML
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
-            # self.fields[field].widget.attrs.update({'placeholder': field})
