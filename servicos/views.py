8c9a30fd292a473a7bf7598709f910c5aa5b1946
Vinicius Hessel <vgerlachhessel@gmail.com>
2023-08-08 20:54:43 -0300

    atualizacao servico

--- servicos/views.py
+++ servicos/views.py
@@ -28,10 +28,23 @@ def servico(request, identificador):
     return render(request, 'servico.html', {'form': form, 'identificador': identificador})
 
 def atualizar_servico(request, identificador):
-    form = FormServico(request.POST)
+    servico = get_object_or_404(Servico, identificador=identificador)
+    form = FormServico(instance=servico)
 
-    if form.is_valid():
-        form.save()
-        return HttpResponse('Salvo com sucesso')
-    else:
-        return render(request, 'novo_servico.html', {'form': form})    
+    if(request.method == 'POST'):
+        form = FormServico(request.POST, instance=servico)
+
+        if(form.is_valid()):
+            servico = form.save(commit=False)
+            servico.servico = form.cleaned_data['servico']
+            servico.cliente = form.cleaned_data['cliente']
+            servico.preco = form.cleaned_data['preco']
+            servico.data_inicio = form.cleaned_data['data_inicio']
+            servico.data_entrega = form.cleaned_data['data_entrega']
+            servico.descricao = form.cleaned_data['descricao']
+            servico.finalizado = form.cleaned_data['finalizado']
+            servico.protocolo = form.cleaned_data['protocolo']
+            servico.save()
+            return HttpResponse('Salvo com sucesso')
+        else:
+            return render(request, 'servico.html', {'form': form, 'identificador': identificador})   
